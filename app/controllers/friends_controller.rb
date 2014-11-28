bf89202d7c873cea4a236ae2ec49fbb3e12777af
Michael Teevan <michael.teevan@gmail.com>
2014-11-28 15:45:42 -0800

    not close to finishing friends, pulling down master

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,15 +1,26 @@
 class FriendsController < ApplicationController
 
   def index
+    user = User.find_by(invitee_id: )
+    @friends = Friend.where(user_id)
+    respond_to do |f|
+      f.json { render json: @friends.to_json }
+    end
   end
 
   def create
+    @create_friend =
   end
 
   def new
   end
 
   def destroy
+  end
+
+  private
+  def question_params
+    params.require(:friend).permit(:inviter_id, :invitee_id)
   end
 
 end
