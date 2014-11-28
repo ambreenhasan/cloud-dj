d6584bf77d1d927eaa41cb7e4ac6320ff6cb5e6c
Michael Teevan <michael.teevan@gmail.com>
2014-11-28 14:57:26 -0800

    removed devise stuff for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  # devise_for :users
-
   resources :users do
     resources :friends, only: [:index, :create, :new, :destroy]
     resources :room_invites, only: [:index, :create, :new, :destroy]
