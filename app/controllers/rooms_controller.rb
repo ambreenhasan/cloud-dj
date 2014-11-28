008c8e7496d38e4efd7fdddd35112a593a6670bf
Daniel Yee <yeedan@gmail.com>
2014-11-28 15:28:47 -0800

    room controller methods

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,24 +1,48 @@
 class RoomsController < ApplicationController
 
   def index
+    @rooms = Room.all
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to @room
+    else
+      render new_user_room
+    end
   end
 
   def new
+    @room = Room.new
   end
 
   def edit
+    @room = Room.find(params[:id])
   end
 
   def show
+    @room = Room.find(params[:id])
   end
 
   def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to @room
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to user_rooms_path
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, :description, :private, :moderator)
   end
 
 end
