e477866229ef4eaf240641bd58d9b2c4cf03cdb3
Alex Trost <alexxtrostt@gmail.com>
2014-11-29 12:12:07 -0800

    created room template and soundcloud controller

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -1,11 +1,13 @@
-angular
-    .module('AngularRails', [
-        'ngRoute',
-        'templates'
-    ]).config(function ($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'home.html',
-                controller: 'HomeCtrl'
-        });
+var app = angular.module('AngularRails', ['ngRoute','templates'])
+
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'HomeCtrl'
+    })
+    .when('/room', {
+        templateUrl: 'room.html',
+        controller: 'RoomCtrl'
     });
+});
