71278649699f26d4287a4fd53acf2ba261f34b14
Alex Trost <alexxtrostt@gmail.com>
2014-11-29 20:03:59 -0800

    working on getting the songs to side down

--- app/assets/javascripts/ng-app/controllers/soundcloud.js
+++ app/assets/javascripts/ng-app/controllers/soundcloud.js
@@ -2,21 +2,38 @@ app.controller("SoundcloudCtrl", ["$scope", "$http", function($scope, $http) {
   // $scope.email = "";
   // $scope.first_name = "";
   // $scope.last_name = "";
+  $scope.songs = [1, 2, 3];
   // $scope.password = "";
+  // $scope.renderSongs = function(data) {
+  //   debugger;
+  //   $scope.songs = data;
+  //   alert(data)
+  // };
+
   $scope.submit = function(search) {
     $http.post("/soundcloud/search", {
       query: search.query
     }).success(function(data){
       renderSongs(data);
+      // $scope.renderSongs(data);
       clearForm()
     })
+  }
+
+
+  function renderSongs(data) {
+    // debugger;
+    for (var i=0; i < 6; i++) {
+      $("#query_song_list").append("<li>" + data[i].attachments_uri + "</li>")
+      console.log(data[i]);
+      $("#query_song_list").slideDown();
+    }
   }
 
   function clearForm() {
     $('#song_search_bar').val("");
   }
 
-  function renderSongs(data) {
-    alert("yaya!")
-  }
+
+
 }]);
--- app/controllers/soundcloud_controller.rb
+++ app/controllers/soundcloud_controller.rb
@@ -5,7 +5,7 @@ class SoundcloudController < ApplicationController
     query = params[:query]
     p "&" *70
     client = SoundCloud.new(:client_id => "90bd68b0531687cfd7e24ede5bb5c013")
-    tracks = client.get('/tracks', :q => query, :limit => 10)
+    tracks = client.get('/tracks', :q => query, :limit => 6)
     # tracks.each do |track|
     #   puts track
     # end
