8f999de162fbeef9d6f20f1596a7d29afaa86167
Alex Trost <alexxtrostt@gmail.com>
2014-11-30 14:06:28 -0800

    making the sign up menu slide down

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -4,13 +4,22 @@ app.controller('HomeCtrl', function ($scope) {
 
    $("#hide_button").on("click", function() {
         if ( $("#login_menu").css('top') != '0px' ) {
-        $("#login_menu").animate({top: 0}, 500);
-        $("#hide_button").html("hide");
+          $("#login_menu").animate({top: 0}, 500);
+          $("#hide_button").html("hide");
         } else  {
           $("#login_menu").animate({top: -178}, 500);
           $("#hide_button").html("login");
         }
     });
 
+   $("#show_signup_form_button").on("click", function() {
+      if ( $("#signup_menu").css('display') === 'none' ) {
+        $("#signup_menu").slideDown(500);
+        $("#show_signup_form_button").html("");
+      } else  {
+        $("#signup_menu").slideUp(500);
+        $("#show_signup_form_button").html("Not a member?");
+      }
+   })
 
 });
