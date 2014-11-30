a3b22496fbe1f98fa11a9207f53ebff2b1bedf88
Alex Trost <alexxtrostt@gmail.com>
2014-11-30 13:42:22 -0800

    added button for login to slide down, not yet functional

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -2,20 +2,6 @@ var app = angular.module('AngularRails')
 
 app.controller('HomeCtrl', function ($scope) {
 
-    // $("#login_menu").toggle(
-    //   function() {$("#login_menu").css("background-color", "blue");},
-    //   function() {$("#login_menu").css("background-color", "green");
-    // }).click();
-
-
-  //  $("#login_menu").on("click", function() {
-  //   $("#login_menu").slideToggle(function() {
-  //     $("#login_menu").animate({top: -90}, 500);
-  //   },function(){
-  //     $("#login_menu").animate({top: 90}, 500)
-  //   });
-  // });
-
    $("#login_menu").on("click", function() {
         if ( $("#login_menu").css('top') != '0px' ) {
         $(this).animate({top: 0}, 500);
