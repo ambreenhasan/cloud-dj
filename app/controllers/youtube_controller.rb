9625dcd7e61fc039e56b85dfe6e698a2a25bf257
Ambreen Hasan <ambodumbo@gmail.com>
2014-11-29 22:33:40 -0800

    added functionality to youtube#search

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -1,14 +1,16 @@
+require 'youtube_it'
+
 class YoutubeController < ApplicationController
 
   def search
+    client = YouTubeIt::Client.new(:dev_key => "AIzaSyAnhBoT9bDXfFK7hOJ6faQhb8lD45Tza8o")
+    videos = client.videos_by(:query => "penguin")
+
+    render json: videos
     # p params
     # query = params[:query]
     # p "&" *70
     # client = SoundCloud.new(:client_id => "90bd68b0531687cfd7e24ede5bb5c013")
-    # tracks = client.get('/tracks', :q => query, :limit => 6)
-    # # tracks.each do |track|
-    # #   puts track
-    # # end
     # render json: tracks
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   # get '*path' => 'application#index'
 
   root 'application#index' #don't remove these two lines
-  # root 'soundcloud#search' #don't remove these two lines
   get '*path' => 'application#index' #don't remove these two lines
 
 
