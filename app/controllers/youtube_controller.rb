654a6811f575e52d0f0bdd387fb949c7a94211c7
Ambreen Hasan <ambodumbo@gmail.com>
2014-11-29 22:44:19 -0800

    youtube rails controller is successfully hitting the angular controller

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -3,8 +3,7 @@ class YoutubeController < ApplicationController
   def search
     query = params[:query]
     client = YouTubeIt::Client.new(:dev_key => "AIzaSyAnhBoT9bDXfFK7hOJ6faQhb8lD45Tza8o")
-    videos = client.videos_by(:query => "penguin")
-
+    videos = client.videos_by(:query => query)
     render json: videos
   end
 end
