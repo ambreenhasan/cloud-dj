dfa71d9845a6a5706c777609ae6b0aa9e38994d6
Alex Trost <alexxtrostt@gmail.com>
2014-11-30 13:20:18 -0800

    stupid animation for showing login works.

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,5 +1,28 @@
 var app = angular.module('AngularRails')
 
 app.controller('HomeCtrl', function ($scope) {
-  $scope.things = ['Angular', 'Rails 4.1', 'Working', 'Together!!'];
+
+    // $("#login_menu").toggle(
+    //   function() {$("#login_menu").css("background-color", "blue");},
+    //   function() {$("#login_menu").css("background-color", "green");
+    // }).click();
+
+
+  //  $("#login_menu").on("click", function() {
+  //   $("#login_menu").slideToggle(function() {
+  //     $("#login_menu").animate({top: -90}, 500);
+  //   },function(){
+  //     $("#login_menu").animate({top: 90}, 500)
+  //   });
+  // });
+
+   $("#login_menu").on("click", function() {
+        if ( $("#login_menu").css('top') != '0px' ) {
+        $(this).animate({top: 0}, 500);
+        } else  {
+          $(this).animate({top: -178}, 500);
+        }
+    });
+
+
 });
