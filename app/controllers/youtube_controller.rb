de721a6d45332b70a9c64eee2ae49e117b09035b
Daniel Yee <yeedan@gmail.com>
2014-11-30 19:54:01 -0800

    youtube response

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -3,8 +3,17 @@ class YoutubeController < ApplicationController
   def search
     query = params[:search][:query]
     client = YouTubeIt::Client.new(:dev_key => "AIzaSyAnhBoT9bDXfFK7hOJ6faQhb8lD45Tza8o")
-    @videos = client.videos_by(:query => query)
-    p @videos
-    # render json: videos
+    videos = client.videos_by(:query => query)
+    @titles = []
+    @video_ids = []
+    @descriptions = []
+    videos.videos.each do |v|
+      @titles << v.title
+      @video_ids << v.video_id
+      @descriptions << v.description
+    end
+    respond_to do |format|
+      format.js { render :youtube }
+    end
   end
 end
