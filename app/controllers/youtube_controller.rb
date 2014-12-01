7d854e3d6d2cf628d7a8da9b3133b090e510e06a
Daniel Yee <yeedan@gmail.com>
2014-11-30 18:18:27 -0800

    ugly youtube search form partial

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -1,9 +1,10 @@
 class YoutubeController < ApplicationController
 
   def search
-    query = params[:query]
+    query = params[:search][:query]
     client = YouTubeIt::Client.new(:dev_key => "AIzaSyAnhBoT9bDXfFK7hOJ6faQhb8lD45Tza8o")
-    videos = client.videos_by(:query => query)
-    render json: videos
+    @videos = client.videos_by(:query => query)
+    p @videos
+    # render json: videos
   end
 end
