a62d0979dc7214c2e14849d3f3dc2c4aace6a734
Ambreen Hasan <ambodumbo@gmail.com>
2014-12-01 16:22:58 -0800

    adding soundcloud methods

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,4 +1,11 @@
 $(document).on("page:change", function(){
 
+  SC.initialize({
+    client_id: "YOUR_CLIENT_ID",
+  });
 
-})
+  SC.stream("/tracks/293", function(sound){
+    sound.play();
+  });
+
+});
