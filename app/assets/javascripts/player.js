1d83c02c3929a023da2bd748aecb8b160742f689
Ambreen Hasan <ambodumbo@gmail.com>
2014-12-01 17:00:35 -0800

    song is playing

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -4,14 +4,13 @@ $(document).on("page:change", function(){
     client_id: "90bd68b0531687cfd7e24ede5bb5c013",
   });
 
-  $(".track_title").attr("id")
 
-  SC.stream("/tracks/", function(sound){
+  $("#queue_container").on("click", ".track_title", function(){
+    var currentItem = $("#queue_container .track_title").attr("id")
+    SC.stream("/tracks/" + currentItem, function(sound){
     sound.play();
   });
 
-  $("queue_container")
+  })
 
 });
-
-// get id
