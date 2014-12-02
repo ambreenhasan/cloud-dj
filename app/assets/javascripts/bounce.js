b5adf2567d304ae7596c9d24ede4ad5cf9b2affa
Alex Trost <alexxtrostt@gmail.com>
2014-12-01 21:07:53 -0800

    just add class bounce and it'll work

--- app/assets/javascripts/bounce.js
+++ app/assets/javascripts/bounce.js
@@ -2,13 +2,9 @@ $(document).ready(function() {
   $.fn.exBounce = function(){
       var self = this;
       (function runEffect(){
-          self.effect("bounce", { times:3 }, 1000, runEffect);
+          self.effect("bounce", { times:3 }, 1500, runEffect);
       })();
      return this;
   };
-
- $(".bouncey").exBounce();
-
-
-
+ $(".bounce").exBounce();
 })
