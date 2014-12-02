52b166d1847680358a2c58e5ee5e99ec531c3708
Alex Trost <alexxtrostt@gmail.com>
2014-12-02 11:32:10 -0800

    weirdly successful ajax request

--- app/assets/javascripts/joinRoom.js
+++ app/assets/javascripts/joinRoom.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
   $(".item").on("click", function() {
     var id = this.id
     alert(id)
+
+    $.ajax ({
+      url: "/users",
+      type: "GET"
+    }).done(function() {alert("win")}).fail(function(){alert("fail")})
   })
 
 
