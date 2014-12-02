2870005a1d4f77919b5d3342c716b91f6bad1d7a
Ambreen Hasan <ambodumbo@gmail.com>
2014-12-02 10:02:21 -0800

    adding psuedocode for player and queue functionality

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -4,7 +4,6 @@ $(document).on("page:change", function(){
     client_id: "90bd68b0531687cfd7e24ede5bb5c013",
   });
 
-
   $("#queue_container").on("click", ".track_title", function(){
     var currentItem = $("#queue_container .track_title").attr("id")
     SC.get("/tracks/" + currentItem, function(track){
--- app/assets/javascripts/queue.js
+++ app/assets/javascripts/queue.js
@@ -1,19 +1,36 @@
 $(document).on("page:change", function(){
-  $("#query_song_list").on("click", ".track_title", function() {
-    var $deleteListTag = $(this).parent();
-    $("#queue_container ul").append($(this))
-    $deleteListTag.remove()
-  })
 
-  $("#queue_container").on("click", ".track_title", function() {
-    var title = $(this).attr('data-track-title');
-    var vid_id = $(this).attr('video_id');
-    var identifier = vid_id.substr(-11,11);
-    var add_html = "<iframe width='480' height='390' frameborder='0' allowfullscreen src='http://www.youtube.com/embed/"+identifier+"?rel=0&autoplay=1' ></iframe>";
+  var queue = [];
 
-    $("#video_container").html(add_html);
-    $('#currently_playing').text(title);
-  });
+
+
+
+  // $("#query_song_list").on("click", ".track_title", function() {
+  //   var $deleteListTag = $(this).parent();
+  //   $("#queue_container ul").append($(this))
+  //   $deleteListTag.remove()
+  // })
+
+  // $("#queue_container").on("click", ".track_title", function() {
+  //   var title = $(this).attr('data-track-title');
+  //   var vid_id = $(this).attr('video_id');
+  //   var identifier = vid_id.substr(-11,11);
+  //   var add_html = "<iframe width='480' height='390' frameborder='0' allowfullscreen src='http://www.youtube.com/embed/"+identifier+"?rel=0&autoplay=1' ></iframe>";
+
+  //   $("#video_container").html(add_html);
+  //   $('#currently_playing').text(title);
+  // });
 });
+
+// add a queue array
+// get elements and push objects into queue that are put into
+// queue container
+// have controls for when a song is finished playing
+// the next song in the queue plays
+// if another song is clicked, the song before it is
+// songs can only be played one after the other
+// moderator can skip songs
+
+// add data types
 
 
