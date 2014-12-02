dcfa682dd1f5695008989793c6ee77a6392ef96e
Daniel Yee <yeedan@gmail.com>
2014-12-01 17:49:35 -0800

    now playing shows the title

--- app/assets/javascripts/queue.js
+++ app/assets/javascripts/queue.js
@@ -6,10 +6,14 @@ $(document).on("page:change", function(){
   })
 
   $("#queue_container").on("click", ".track_title", function() {
+    var title = $(this).attr('data-track-title');
     var vid_id = $(this).attr('video_id');
     var identifier = vid_id.substr(-11,11);
     var add_html = "<iframe width='480' height='390' frameborder='0' allowfullscreen src='http://www.youtube.com/embed/"+identifier+"?rel=0&autoplay=1' ></iframe>";
+
     $("#video_container").html(add_html);
+
+    $('#currently_playing').text(title);
   })
 })
 
