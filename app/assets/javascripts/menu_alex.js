acb2ede0bd9143084826d150318f893f3cf7f077
Alex Trost <alexxtrostt@gmail.com>
2014-12-03 09:28:56 -0800

    looking really nice so far

--- app/assets/javascripts/menu_alex.js
+++ app/assets/javascripts/menu_alex.js
@@ -21,6 +21,21 @@ $(document).ready(function() {
     defaultBgColor      : '#fff'
   };
 
+
+    // var $el             = $(this),
+    //   $menuItems      = $el.children('li'),
+    // maxdelay    = Math.max( settings.animMouseleave['mText'].speed + settings.animMouseleave['mText'].delay ,
+    //     settings.animMouseleave['sText'].speed + settings.animMouseleave['sText'].delay ,
+    //     settings.animMouseleave['icon'].speed + settings.animMouseleave['icon'].delay
+    //   ),
+    //   t_mouseenter;
+    // $menuItems.find('.sti-item').each(function() {
+    //   var $el = $(this);
+    //   $el.data('deftop', $el.position().top);
+    // });
+
+
+
   function handlerInMenu() {
     var $item       = $(this),
         $wrapper    = $item.children('a'),
@@ -29,45 +44,44 @@ $(document).ready(function() {
         hovercolor  = $item.data('hovercolor');
     $item.addClass('sti-current');
 
-    $movingItems.each(function(i) {
-        var $item           = $(this),
-        item_sti_type   = $item.data('type'),
-        speed           = settings.animMouseenter[item_sti_type].speed,
-        easing          = settings.animMouseenter[item_sti_type].easing,
-        delay           = settings.animMouseenter[item_sti_type].delay,
-        dir             = settings.animMouseenter[item_sti_type].dir,
-        // if dir is 1 the item moves downwards
-        // if -1 then upwards
-        style           = {'top' : -dir * wrapper_h + 'px'};
 
-        if( item_sti_type === 'icon' ) {
-            // this sets another bg image position for the icon
-          style.backgroundPosition    = 'bottom left';
-            } else {
-              style.color                 = hovercolor;
-            }
-            // we hide the icon, move it up or down, and then show it
-            $item.hide().css(style).show();
-            clearTimeout($item.data('time_anim'));
-            $item.data('time_anim',
-              setTimeout(function() {
-                // now animate each item to its default tops
-                // each item will animate with a delay specified
-                // in the options
-                $item.stop(true)
-                    .animate({top : $item.data('deftop') + 'px'}, speed, easing);
-            }, delay)
-                );
-    });
+      $wrapper.animate({"background-color": "000"}, 300);
+      $movingItems.animate({"color": "purple"}, 300);
+      $wrapper.animate({"top": "-15px"}, 200);
 
-    $wrapper.stop(true).animate({
-        backgroundColor: settings.defaultTextColor
-        }, settings.boxAnimSpeed );
+    // $movingItems.each(function(i) {
+    //     var $item           = $(this),
+    //     item_sti_type   = $item.data('type'),
+    //     speed           = settings.animMouseenter[item_sti_type].speed,
+    //     easing          = settings.animMouseenter[item_sti_type].easing,
+    //     delay           = settings.animMouseenter[item_sti_type].delay,
+    //     dir             = settings.animMouseenter[item_sti_type].dir,
+
+    //     style           = {'top' : -dir * wrapper_h + 'px'};
+
+        // if( item_sti_type === 'icon' ) {
+        //   style.backgroundPosition    = 'bottom left';
+        //     } else {
+        //       style.color                 = hovercolor;
+        //     }
+            // $item.hide().css(style).show();
+            // clearTimeout($item.data('time_anim'));
+            // $item.data('time_anim',
+              // setTimeout(function() {
+                // $item.stop(true)
+                    // .animate({top : $item.data('deftop') + 'px'}, speed, easing);
+            // }, delay)
+                // );
 
 
+    // });
 
-
+    // $wrapper.stop(true).animate({
+    //     backgroundColor: settings.defaultTextColor
+    //     }, settings.boxAnimSpeed );
   }
+
+
 
   function handlerOutMenu() {
     var $item       = $(this),
@@ -76,13 +90,41 @@ $(document).ready(function() {
         $movingItems= $wrapper.find('.sti-item'),
         hovercolor  = $item.data('hovercolor');
     $item.removeClass('sti-current');
+
+    $wrapper.animate({"background-color": "#fff"}, 500);
+    $movingItems.animate({"color": "#000"}, 500);
+    $wrapper.animate({"top": "0px"}, 200);
+
+    // $movingItems.each(function(i) {
+    //   var $item           = $(this),
+    //     item_sti_type   = $item.data('type'),
+    //     speed           = settings.animMouseleave[item_sti_type].speed,
+    //     easing          = settings.animMouseleave[item_sti_type].easing,
+    //     delay           = settings.animMouseleave[item_sti_type].delay,
+    //     dir             = settings.animMouseleave[item_sti_type].dir;
+    //   clearTimeout($item.data('time_anim'));
+    //   setTimeout(function() {
+    //     $item.stop(true).animate({'top' : -dir * wrapper_h + 'px'}, speed, easing, function() {
+    //       if( delay + speed === maxdelay ) {
+    //         $wrapper.stop(true).animate({
+    //           backgroundColor: settings.defaultBgColor
+    //         }, settings.boxAnimSpeed );
+    //         $movingItems.each(function(i) {
+    //           var $el             = $(this),
+    //             style           = {'top' : $el.data('deftop') + 'px'};
+    //           if( $el.data('type') === 'icon' ) {
+    //             style.backgroundPosition    = 'top left';
+    //           } else {
+    //             style.color                 = settings.defaultTextColor;
+    //           }
+    //           $el.hide().css(style).show();
+    //         });
+    //       }
+    //     });
+    //   }, delay);
+    // })
+
   }
-
-
-
-
-
-
 
 
 
