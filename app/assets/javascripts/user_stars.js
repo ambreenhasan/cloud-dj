0447f5d35545a0105ce9343cdb353bfb7eb8967c
Ambreen Hasan <ambodumbo@gmail.com>
2014-12-03 11:56:19 -0800

    made changes to the objects being grabbed in the DOM

--- app/assets/javascripts/user_stars.js
+++ app/assets/javascripts/user_stars.js
@@ -2,9 +2,8 @@ $(document).on("page:change", function(){
   $("#fav_button").on("click", function(event){
     if ($(this).is(":checked")) {
       var userid = $("#sessionuserid").attr("sessionid")
-      var apiid = 'unnecessary'//hardcoded, not needed anymore?
-      var songtitle = $("#fav_button").attr("title")
-      var songdesc = $("#fav_button").attr("description")
+      var apid = $("#queue_container ul").first().attr("id")
+      var songtitle = $("#queue_container ul").first().attr("data-title-title")
       var pass_to_songs = {song: {user_id: userid, api_id: apiid, title: songtitle, description: songdesc}}
 
       var songid = 0
@@ -15,7 +14,7 @@ $(document).on("page:change", function(){
 
       function save_song() {
         $.ajax({
-          url: '/users/'+userid+'/songs',
+          url: '/users/'+ userid +'/songs',
           type: 'post',
           data: pass_to_songs,
           async: false
@@ -31,7 +30,7 @@ $(document).on("page:change", function(){
 
       function save_user_star() {
         $.ajax({
-          url: '/users/'+userid+'/user_stars',
+          url: '/users/' + userid + '/user_stars',
           type: 'post',
           data: pass_to_user_stars
         })
