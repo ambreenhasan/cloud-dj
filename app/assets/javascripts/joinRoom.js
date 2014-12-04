053327a2a0344a8c9e399262860311c1f27b0424
Alex Trost <alexxtrostt@gmail.com>
2014-12-03 20:53:18 -0800

    cool transistion from profile to room happening

--- app/assets/javascripts/joinRoom.js
+++ app/assets/javascripts/joinRoom.js
@@ -15,7 +15,21 @@ $(document).ready(function() {
     $("#ajax_home_thing").css("display", "none");
     $("#profile_page").css("display", "none");
     $("#room").slideDown(1000)
+    $("#trending_carousel_div").css("display", "none");
+    $("#home_trending_section").animate({"top": "19em"}, 500);
+    $(".sti-menu").animate({"top": "3em"}, 500);
+    $("#home_trending_section").animate({"height": "28em"}, 500);
+    $("#home_trending_section").addClass("hoverUp");
   }
+
+  $(".hoverUp").on("mouseover", function() {
+    $(this).animate({"top": 0},500);
+  })
+
+  $(".hoverUp").on("mouseleave", function() {
+    if $(this).animate({"top": 0},500);
+  })
+
 
 });
 
