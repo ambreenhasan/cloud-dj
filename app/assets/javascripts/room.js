549cb8b9defcb89dc3e664b6d4dc1c678a379b15
Alex Trost <alexxtrostt@gmail.com>
2014-12-03 21:33:31 -0800

    butt ugly code

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -1,21 +1,14 @@
-function roomOutTransition() {
-  alert("hit")
-  $("#room").css("display", "none");
-  $(".sti-menu").animate({"top": 0}, 500);
-  $("#trending_carousel_div").css("display", "block");
-  $("#home_trending_section").animate({"top": 0}, 500);
-  $("#home_trending_section").animate({"height": "960px"}, 500);
-}
 
 
 $(document).ready(function() {
 
-  if ($("#room").css("display") === "block") {
-    $(".sti-menu li").on("click", function() {
-      roomOutTransition()
-    })
-  }
-
+    function roomOutTransition() {
+      $("#room").css("display", "none");
+      $(".sti-menu").animate({"top": 0}, 500);
+      $("#trending_carousel_div").css("display", "block");
+      $("#home_trending_section").animate({"top": 0}, 500);
+      $("#home_trending_section").animate({"height": "960px"}, 500);
+    }
 
   $('#hide_button').on("click", function() {
         if ( $("#login_menu").css('top') != '0px' ) {
@@ -32,6 +25,7 @@ $(document).ready(function() {
         $("#signup_menu").slideDown(500);
         $("#show_signup_form_button").html("hide registration form.");
         $("#hide_button").css("display", "none");
+          roomOutTransition()
       } else  {
         $("#signup_menu").slideUp(500);
         $("#show_signup_form_button").html("Not a member?");
@@ -44,6 +38,7 @@ $(document).ready(function() {
         $("#new_room_form_div").slideDown(500);
         $("#ajax_home_thing").css("display", "none");
         $('#profile_page').css("display", "none");
+
       } else
         $("#new_room_form_div").slideUp(500);
         $("#new_room_tab").css("color", "#080308");
@@ -61,7 +56,10 @@ $(document).ready(function() {
       $("#room").css("display", "none");
       $("#new_room_form_div").css("display", "none");
       $('#profile_page').css("display", "none");
+      $("#room").css("display", "none");
       $("#ajax_home_thing").slideDown();
+      roomOutTransition();
+
     })
 
       $(".bounce").exBounce();
