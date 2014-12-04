cca29df9a220015bffa440201e6dcf7fb4e7afa8
Alex Trost <alexxtrostt@gmail.com>
2014-12-04 11:22:56 -0800

    fixed bug in user stars controller

--- app/controllers/user_stars_controller.rb
+++ app/controllers/user_stars_controller.rb
@@ -1,6 +1,11 @@
 class UserStarsController < ApplicationController
   def create
+    p params
+    p "222332" * 100
     @user_star = UserStar.new(user_star_params)
+    @user_star.song_id = Song.last.id
+    @user_star.save
+    p @user_star
     render json: @user_star if @user_star.save
   end
 
