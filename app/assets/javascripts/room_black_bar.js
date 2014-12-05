5f56f29aac3968bd8e3698b0ffe91562cf1152ca
Alex Trost <alexxtrostt@gmail.com>
2014-12-04 17:27:25 -0800

    div shows up on top of other div now

--- app/assets/javascripts/room_black_bar.js
+++ app/assets/javascripts/room_black_bar.js
@@ -2,12 +2,12 @@
 
  $("#room_black_bar").on("mouseover", function() {
       $("#room_black_bar").animate({"padding-top": "-10em"},600);
-      $("#room_black_bar").animate({"margin-top": "3em"},600);
+      $("#room_black_bar").animate({"margin-top": "12em"},600);
     })
 
   $("#room_black_bar").on("mouseleave", function() {
       $("#room_black_bar").animate({"padding-top": "10em"},600);
-      $("#room_black_bar").animate({"margin-top": "17em"},600);
+      $("#room_black_bar").animate({"margin-top": "23em"},600);
     })
 
 })
